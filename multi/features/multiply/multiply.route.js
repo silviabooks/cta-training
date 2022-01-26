76ff25a9352e107f38604ee8372002c3b4ca1d24
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-01-26 17:02:47 +0000

    Add weird validation rule

--- multi/features/multiply/multiply.route.js
+++ multi/features/multiply/multiply.route.js
@@ -1,6 +1,16 @@
 const multiplyRoute = (request, reply) => {
   console.log(request.body);
 
+  if (request.body.input.num1 === 5) {
+    reply.status(418).send({
+      message: 'we do not like n.5',
+      extensions: {
+        path: "$.input.num1",
+        code: 123
+      },
+    })
+  }
+
   const result = request.body.input.num1 * request.body.input.num2;
 
   reply.send({ result });
