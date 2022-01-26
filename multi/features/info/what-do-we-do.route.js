2c6d5b559412f89cff0481ae1919ce0336d46310
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-01-26 15:47:08 +0000

    Add more routes

---
+++ multi/features/info/what-do-we-do.route.js
@@ -0,0 +1,3 @@
+const whatDoWeDoRoute = (request, reply) => reply.send('We do cool stuff');
+
+module.exports = whatDoWeDoRoute;
---
+++ multi/features/info/who-are-we.route.js
@@ -0,0 +1,3 @@
+const whoAreWeRoute = (request, reply) => reply.send('We are cool');
+
+module.exports = whoAreWeRoute;
