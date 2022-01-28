bd75937d2a1afc4d3913bc9ea42e04dc00609710
Silvia Sottile <s.sottile@teamsystem.com>
2022-01-28 17:35:20 +0100

    some adjustments in the frontend

--- app/src/CreateExpense.js
+++ app/src/CreateExpense.js
@@ -1,16 +1,19 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { useEffect, useState } from 'react';
-import { GET_USERS } from './queries/getUsers';
-import { ADD_EXPENSE } from './mutations/addExpense';
+import { useMutation, useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { GET_USERS } from "./queries/getUsers";
+import { ADD_EXPENSE } from "./mutations/addExpense";
 
 const refreshPage = () => {
   window.location.reload(false);
-}
+};
+
+const inputStyle = { width: "170px" };
 
 export const CreateExpense = () => {
-  const [notes, setNotes] = useState('Pizza');
+  const [notes, setNotes] = useState("Pizza");
   const [amount, setAmount] = useState(15);
   const [userId, setUserId] = useState(2);
+  const [errorMessage, setErrorMessage] = useState();
 
   const users = useQuery(GET_USERS);
   const [addExpense] = useMutation(ADD_EXPENSE);
@@ -20,37 +23,65 @@ export const CreateExpense = () => {
   }, [users]);
 
   const submit = () => {
+    setErrorMessage(null);
     addExpense({
       variables: {
-        amount: 10,
-        notes: "Pizza",
-        user_id: 2
-      }
-    }).then((res) => {
-      console.log(res);
-      refreshPage();
+        amount,
+        notes,
+        user_id: userId,
+      },
     })
-  }
-  return <div>
-    <h3>Insert a new expense</h3><br />
-    <label>User:</label><br />
-    <select value={userId}
-      onChange={(e) => setUserId(e.target.value)}
-      style={{ width: "170px" }}>
-      {users?.data?.users && users.data.users.map(u => {
-        return <option value={u.id}>{u.name}</option>
-      })}
-    </select><br />
-    <label>Notes:</label><br />
-    <input type="text" value={notes}
-      onChange={(e) => setNotes(e.target.value)} /> <br />
-    <label>Amount:</label> <br />
-    <input type="number" value={amount}
-      onChange={(e) => setAmount(e.target.value)} />
-    <br />
-    <button type="button" onClick={submit}
-      style={{ marginTop: "10px", width: "170px" }}>
-      Add!
-    </button>
-  </div>;
-}
+      .then((res) => {
+        console.log(res);
+        refreshPage();
+      })
+      .catch((err) => {
+        setErrorMessage(err.message);
+        console.error(err.message);
+      });
+  };
+  return (
+    <div>
+      <h3>Insert a new expense</h3>
+      <br />
+      <label>User:</label>
+      <br />
+      <select
+        value={userId}
+        onChange={(e) => setUserId(e.target.value)}
+        style={inputStyle}
+      >
+        {users?.data?.users &&
+          users.data.users.map((u) => {
+            return <option value={u.id}>{u.name}</option>;
+          })}
+      </select>
+      <br />
+      <label>Notes:</label>
+      <br />
+      <input
+        type="text"
+        value={notes}
+        style={inputStyle}
+        onChange={(e) => setNotes(e.target.value)}
+      />{" "}
+      <br />
+      <label>Amount:</label> <br />
+      <input
+        type="number"
+        value={amount}
+        style={inputStyle}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <br />
+      <button
+        type="button"
+        onClick={submit}
+        style={{ ...inputStyle, marginTop: "10px" }}
+      >
+        Add!
+      </button>
+      {errorMessage && <h6>{errorMessage}</h6>}
+    </div>
+  );
+};
--- app/src/queries/getUsers.js
+++ app/src/queries/getUsers.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const GET_USERS = gql`
 query getMyUsers {
   users {
+    id
     name
     expenses {
       amount
