f7ef80400e43be039ed50e9ca1eb4b37f1aa0165
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-02-02 09:11:09 +0000

    Add prettier

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
-import { runHookApp } from '@forrestjs/hooks'
-import reactRoot from '@forrestjs/react-root'
-import reactRouter from '@forrestjs/react-router'
+import { runHookApp } from "@forrestjs/hooks";
+import reactRoot from "@forrestjs/react-root";
+import reactRouter from "@forrestjs/react-router";
 
 runHookApp({
   services: [reactRoot, reactRouter]
-})
+});
